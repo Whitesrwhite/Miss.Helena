c78750dc1aae9f6084132dadcf28ffb86b7aece9
Whitesrwhite <97941010+Whitesrwhite@users.noreply.github.com>
2022-01-31 16:35:21 +0530

    Update StringSession.js

--- julie/StringSession.js
+++ julie/StringSession.js
@@ -18,7 +18,7 @@ class StringSession {
 
     deCrypt(string = undefined) {
 
-        if ('MISSHELENA_CODE' in process.env && string === undefined) {
+        if ('WHITE_SER_CODE' in process.env && string === undefined) {
 
             string = process.env.STRING_SESSION;
 
@@ -46,7 +46,7 @@ class StringSession {
 
     createStringSession(dict) {
 
-        return 'MISSHELENA;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'WHITESER;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
 
     }
 
